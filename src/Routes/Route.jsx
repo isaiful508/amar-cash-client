960ed8a3d0529fb6d4da50bf4568873647fab0b7
isaiful508 <isaiful.islam508@gmail.com>
2024-07-16 00:06:21 +0600

    Alhamdulillah set up done

---
+++ src/Routes/Route.jsx
@@ -0,0 +1,14 @@
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+    },
+  ]);
+  export default router;
